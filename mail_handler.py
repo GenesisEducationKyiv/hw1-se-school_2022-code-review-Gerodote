6a484301421483eaea22a0f9433eeda5f1d811a7
Gerodote <lg3dx6fd@gmail.com>
2022-08-31 00:40:45 +0300

    pep8 adapted

--- mail_handler.py
+++ mail_handler.py
@@ -3,23 +3,23 @@ from abc import ABC, abstractmethod
 import gmail_API
 
 
-class Abstract_mail_handler(ABC):
+class AbstractMailHandler(ABC):
     @abstractmethod
     def send_plain_messages_to_emails(self,
-                                            list_subscribed_emails,
-                                            subject_text,
-                                            message_plain_text):
+                                      list_subscribed_emails,
+                                      subject_text,
+                                      message_plain_text):
         pass
     
     
-class gmail_handler(Abstract_mail_handler):
+class GmailHandler(AbstractMailHandler):
     def __init__(self,
                  client_secret_file='client_secret.json',
                  api_name='gmail',
                  api_version='v1', 
                  scopes=['https://www.googleapis.com/auth/gmail.send']
                  ):
-        self._gmail_client = gmail_API.basic_gmail_api(client_secret_file,
+        self._gmail_client = gmail_API.BasicGmailAPI(client_secret_file,
                                                        api_name,
                                                        api_version,
                                                        scopes)
@@ -34,12 +34,12 @@ class gmail_handler(Abstract_mail_handler):
                 message_text=message_plain_text
             )
         
-def factory_mail_handler(mode='gmail', *args, **kwargs) -> Abstract_mail_handler:
+def factory_mail_handler(mode='gmail', *args, **kwargs) -> AbstractMailHandler:
         '''
         Use any mode from: "gmail", "SMTP"
         '''
         if mode == 'gmail':
-            return gmail_handler(*args, **kwargs)
+            return GmailHandler(*args, **kwargs)
         # if mode == 'SMTP':
         #     return SMTP_handler(*args, **kwargs)
         raise KeyError(f"such mode either don't exist or didn't implemented.")
@@ -49,5 +49,5 @@ if __name__ == "__main__":
     print("Executing initialization of gmail account.")
     print("!!! WARNING: don't delete this file after configuring gmail API, "+\
         "because this file is also used to run the gmail API on server.")
-    gmail_client = gmail_handler()            
+    gmail_client = GmailHandler()            
             
